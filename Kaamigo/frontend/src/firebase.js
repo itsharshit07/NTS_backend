595a45bdead3fdd02327a7c0762679b08f3509a7
Anuj Karambalkar <anujkarambalkar1504@gmail.com>
2025-07-10 19:52:14 +0530

    reels section done

--- Kaamigo/frontend/src/firebase.js
+++ Kaamigo/frontend/src/firebase.js
@@ -1,16 +1,18 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAYpUMPpKV8xYC1lcieDn8JNAQSElIUT8w",
   authDomain: "kaamigo-ce0e8.firebaseapp.com",
   projectId: "kaamigo-ce0e8",
-  storageBucket: "kaamigo-ce0e8.firebasestorage.app",
+  storageBucket: "kaamigo-ce0e8.appspot.com", // Fixed storage bucket
   messagingSenderId: "667370815778",
   appId: "1:667370815778:web:2b5f974f61c1cfe0e8257b",
-  measurementId: "G-TLV31QBS7J"
+  measurementId: "G-TLV31QBS7J",
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
 export default app;
