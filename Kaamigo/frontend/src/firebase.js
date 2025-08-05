a1ceaac541477104740f3fcc941b14c10488424c
Harshit Gola <golaharsh0411@gmail.com>
2025-08-05 22:43:34 +0530

    local variables

--- Kaamigo/frontend/src/firebase.js
+++ Kaamigo/frontend/src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAYpUMPpKV8xYC1lcieDn8JNAQSElIUT8w",
-  authDomain: "kaamigo-ce0e8.firebaseapp.com",
-  projectId: "kaamigo-ce0e8",
-  storageBucket: "kaamigo-ce0e8.appspot.com", // Fixed storage bucket
-  messagingSenderId: "667370815778",
-  appId: "1:667370815778:web:2b5f974f61c1cfe0e8257b",
-  measurementId: "G-TLV31QBS7J",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
